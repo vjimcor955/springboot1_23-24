cbfe7ee21042e67bc2d4bfefc65d62f56235cfd1
jmsa <tulkas72@gmail.com>
2023-11-03 01:20:30 +0100

    Manejo Errores

---
+++ src/main/java/com/example/demo/error/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
