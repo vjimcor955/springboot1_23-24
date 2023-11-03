1ad44e6226e2201b97d77a9001d10c54c422b487
jmsa <tulkas72@gmail.com>
2023-11-03 01:18:26 +0100

    ProductoControlador completo

--- src/main/java/com/example/demo/controlador/ProductoControlador.java
+++ src/main/java/com/example/demo/controlador/ProductoControlador.java
@@ -1,11 +1,12 @@
 package com.example.demo.controlador;
 
 import com.example.demo.repos.ProductoRepositorio;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.modelo.Producto;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -28,5 +29,29 @@ public class ProductoControlador {
     {
         return productoRepositorio.findById(id).orElse(null);
     }
+    @PostMapping("/")
+    public Producto createProducto(@Valid @RequestBody Producto producto) {
+        return productoRepositorio.save(producto);
+    }
 
+    @PutMapping("/{id}")
+    public Producto updateProducto(@PathVariable Long id, @Valid @RequestBody Producto producto) {
+        return productoRepositorio.findById(id)
+                .map(existingProducto -> {
+                    existingProducto.setName(producto.getName());
+                    existingProducto.setPrice(producto.getPrice());
+                    return productoRepositorio.save(existingProducto);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Producto not found with id " + id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProducto(@PathVariable Long id) {
+        return productoRepositorio.findById(id)
+                .map(producto -> {
+                    productoRepositorio.delete(producto);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Producto not found with id " + id));
+    }
 }
