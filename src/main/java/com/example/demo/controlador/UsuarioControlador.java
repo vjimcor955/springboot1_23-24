e9c3a7a2f968a864685a643f96a7a66f2df957fe
jmsa <tulkas72@gmail.com>
2023-11-03 01:12:46 +0100

    UsuarioControlador completo

--- src/main/java/com/example/demo/controlador/UsuarioControlador.java
+++ src/main/java/com/example/demo/controlador/UsuarioControlador.java
@@ -2,10 +2,12 @@ package com.example.demo.controlador;
 
 import com.example.demo.modelo.Usuario;
 import com.example.demo.repos.UsuarioRepositorio;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -24,5 +26,29 @@ public class UsuarioControlador {
     }
 
     // Add methods for POST, PUT, DELETE...
+    @PostMapping("/")
+    public Usuario createUsuario(@Valid @RequestBody Usuario usuario) {
+        return usuarioRepositorio.save(usuario);
+    }
 
+    @PutMapping("/{id}")
+    public Usuario updateUsuario(@PathVariable Long id, @Valid @RequestBody Usuario usuario) {
+        return usuarioRepositorio.findById(id)
+                .map(existingUsuario -> {
+                    existingUsuario.setName(usuario.getName());
+                    existingUsuario.setEmail(usuario.getEmail());
+                    return usuarioRepositorio.save(existingUsuario);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado  con id " + id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable Long id) {
+        return usuarioRepositorio.findById(id)
+                .map(usuario -> {
+                    usuarioRepositorio.delete(usuario);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario not found with id " + id));
+    }
 }
