42cb94eac6a5b6599e78578fdd7ee524abdcf075
jmsa <tulkas72@gmail.com>
2023-11-03 01:38:27 +0100

    Modificado controlador Producto para asociarlo con el usuario que lo crea.

--- src/main/java/com/example/demo/controlador/ProductoControlador.java
+++ src/main/java/com/example/demo/controlador/ProductoControlador.java
@@ -1,6 +1,7 @@
 package com.example.demo.controlador;
 
 import com.example.demo.repos.ProductoRepositorio;
+import com.example.demo.repos.UsuarioRepositorio;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +14,11 @@ import java.util.List;
 @RequestMapping("/api/producto")
 public class ProductoControlador {
     private final ProductoRepositorio productoRepositorio;
+    private final UsuarioRepositorio usuarioRepositorio;
 
-    public ProductoControlador(ProductoRepositorio productoRepositorio) {
+    public ProductoControlador(ProductoRepositorio productoRepositorio, UsuarioRepositorio usuarioRepositorio) {
         this.productoRepositorio = productoRepositorio;
+        this.usuarioRepositorio = usuarioRepositorio;
     }
 
     @GetMapping("/")
@@ -54,4 +57,31 @@ public class ProductoControlador {
                 })
                 .orElseThrow(() -> new ResourceNotFoundException("Producto not found with id " + id));
     }
+
+    // Crear productos asociados a cliente
+    @PostMapping("/{id}/productos")
+    public Producto addProducto(@PathVariable Long id, @Valid @RequestBody Producto producto) {
+        return productoRepositorio.findById(id)
+                .map(usuario -> {
+                    producto.setUsuario(usuario.getUsuario());
+                    return productoRepositorio.save(producto);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Usuario not found with id " + id));
+    }
+
+    @PutMapping("/{id}/productos/{productoId}")
+    public Producto updateProducto(@PathVariable Long id, @PathVariable Long productoId, @Valid @RequestBody Producto productoRequest) {
+        if(!usuarioRepositorio.existsById(id)) {
+            throw new ResourceNotFoundException("Usuario not found with id " + id);
+        }
+
+        return productoRepositorio.findById(productoId)
+                .map(producto -> {
+                    producto.setName(productoRequest.getName());
+                    producto.setPrice(productoRequest.getPrice());
+                    return productoRepositorio.save(producto);
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Producto not found with id " + productoId));
+    }
+
 }
